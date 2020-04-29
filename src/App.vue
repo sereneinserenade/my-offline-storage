65c8a2bf56baaca716278f00efd531f8c647d72e
Jeet Mandaliya <45892659+sereneinserenade@users.noreply.github.com>
2020-04-29 06:00:50 +0200

    removed helloworld added some style

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,10 @@
 </template>
 
 <style lang="scss">
+* {
+  scroll-behavior: smooth;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Local.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="hello">
+    Hey there, this is my local ap
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Local"
+};
+</script>
